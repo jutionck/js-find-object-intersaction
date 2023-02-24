46fa3223e85987caf41c067dc183a10ae73e0794
jutionck <jutionck@gmail.com>
2023-02-24 18:46:29 +0700

    try: 001

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ dotenv.config();
 const path = process.env.FILE_PATH + "data.json";
 
 readFile(path).then((data) => {
+  const workingHoursEnd = data[0].workingHoursEnd;
+  let count = 1;
   for (const key in data) {
+    if (key === 0 && data[key].workingHoursStart <= data[key].workingHoursEnd) {
+      count = count + 1;
+      continue;
+    }
+
+    if (data[count].workingHoursStart <= workingHoursEnd) {
+      console.log(
+        "key:",
+        key,
+        "workingHoursStart:",
+        data[key].workingHoursStart
+      );
+    }
   }
 });
